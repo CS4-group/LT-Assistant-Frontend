fdaea10ccb1aae52f498737cf2b0b1a8237dbe04
Nav Shrivastava <navsh001@gmail.com>
2026-03-31 15:27:35 -0500

    config change

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
     API_BASE_URL: window.location.hostname === 'localhost'
         ? 'http://localhost:3000'
-        : 'https://lt-assistant-backend-production.up.railway.app',
+        : 'https://api.ltassistant.com',
 };
