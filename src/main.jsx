697949810d8161029bfc74d0641028c4cbc4a003
Nav Shrivastava <navsh001@gmail.com>
2026-04-02 02:06:34 -0500

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,13 @@ import { createRoot } from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+// Redirect /confirm/:token paths to hash-based route so HashRouter can handle them
+const { pathname } = window.location
+const confirmMatch = pathname.match(/^\/confirm\/(.+)/)
+if (confirmMatch) {
+  window.location.replace(`${window.location.origin}/#/confirm/${confirmMatch[1]}`)
+}
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
